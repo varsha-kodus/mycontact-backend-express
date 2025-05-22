90ed2ff8ae9893d406a76e7b6a7fec802065246f
varsha-kodus <varsha.kodus@gmail.com>
2025-05-22 12:44:17 +0530

    change in swagger docs

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,25 +1,21 @@
-require('dotenv').config(); // Load .env variables
 const swaggerAutogen = require('swagger-autogen')();
 
 const PORT = process.env.PORT || 5000;
 
-// Dynamically determine host
 const RAW_HOST = process.env.VERCEL_URL || `localhost:${PORT}`;
-const HOST = RAW_HOST.replace(/^https?:\/\//, ''); // Remove protocol if included
+const HOST = RAW_HOST.replace(/^https?:\/\//, '');
 const SCHEMES = RAW_HOST.includes('localhost') ? ['http'] : ['https'];
 
 const doc = {
   info: {
-    title: 'My Contact App',
+    title: 'My Contact App Test',
     description: 'The Contact App is built using Node.js and Express.'
   },
   host: HOST,
   schemes: SCHEMES
 };
 
-const outputFile = './swagger-output.json';
-
-// ⚠️ Pass ONLY the root route file (entry point like app.js or index.js)
-const routes = ['./routes/userRoutes.js', './routes/contactRoutes.js'];
+const outputFile = './docs/swagger-output.json';
+const routes = ['./server.js']; // <== Point here
 
 swaggerAutogen(outputFile, routes, doc);
