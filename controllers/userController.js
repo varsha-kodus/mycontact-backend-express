129b5a5660b1f627a1e30803856cf7da5b000610
varsha-kodus <varsha.kodus@gmail.com>
2025-05-22 18:03:09 +0530

    Changes in email configuration

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,7 +35,7 @@ const registerUser = asyncHandler( async (req, res)=>{
     });
 
     const mailer = nodemailer.createTransport({
-        host: 'gmail',
+        host: 'smtp.gmail.com',
         port: 465,
         secure: true,
         auth: {
